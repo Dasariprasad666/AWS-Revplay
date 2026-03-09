ad105d474fa4eee6279270bcc76dfa2ce0ce9b4a
Dasariprasad666 <deviprasad99515@gmail.com>
2026-03-09 15:27:15 +0530

    modified

--- backend/src/main/java/com/example/demo/security/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         //  Here is the magic fix! We added your AWS IP address to the VIP list.
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://16.171.141.25"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://13.53.175.84"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8080/api'
-   apiUrl:'http://16.171.141.25:8080/api'
+   apiUrl:'http://13.53.175.84:8080/api'
 };
